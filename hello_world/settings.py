93562eb64718ef921d86a03df91ffca18b0725ea
jjorissen52 <jjorissen52@gmail.com>
2018-03-24 19:38:42 -0500

    added postgres image

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -72,14 +72,16 @@ WSGI_APPLICATION = 'hello_world.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '', # default on postgres docker image is ''
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
